977d7627ed957396788004fd2ac1e504f7d4b3e4
nawar993 <nawar.fr93@gmail.com>
2026-03-15 10:45:32 +0100

    deleted

--- backend/prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-import dotenv from 'dotenv';
-
-dotenv.config(); // لتحميل المتغيرات من .env
-
-// إنشاء Prisma Client
-export const prisma = new PrismaClient();
